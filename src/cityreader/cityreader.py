0f3e83ec901c7fd170d3b7fae2fa1bd2b05fc071
Tyler Alsop <tylerjalsop@gmail.com>
2020-06-12 20:32:16 -0600

    Fixed code to open the cities.csv file to be able to run the test. Test not passing, but it's progress.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,12 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -16,11 +22,20 @@
 # should not be loaded into a City object.
 cities = []
 
+import csv
+
 def cityreader(cities=[]):
+
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  with open('cities.csv') as cities_file:
+    file_reader = csv.reader(cities_file)
+
+    for row in file_reader:
+      added_city = City(row[0], row[3], row[4])
+      cities.append(added_city)
+
     return cities
 
 cityreader(cities)
